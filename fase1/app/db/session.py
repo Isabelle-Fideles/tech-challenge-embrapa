0b2fa2820bd243f8982a15f432874658813f35be
Glauberthy <glauberthy@gmail.com>
2025-05-20 21:11:32 -0300

    fix: Update DATABASE_URL to point to the correct database path

--- fase1/app/db/session.py
+++ fase1/app/db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./embrapa.db"
+DATABASE_URL = "sqlite:///./db_data/embrapa.db"
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
