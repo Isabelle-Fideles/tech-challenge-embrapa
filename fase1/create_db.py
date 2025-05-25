10ed8decf75b8e15dc3bd2ffe12266ce3e25e4f9
Glauberthy <glauberthy@gmail.com>
2025-05-25 16:00:01 -0300

    fix: Remove unused import from create_db.py

--- fase1/create_db.py
+++ fase1/create_db.py
@@ -1,6 +1,5 @@
 from app.db.session import engine
 from app.db.base import Base
-from app.models.scraping import EmbrapaProducao
 
 def create_db_and_tables():
     Base.metadata.create_all(bind=engine)
